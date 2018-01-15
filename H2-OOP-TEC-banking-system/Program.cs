69f5833e7a5ea40bec7e96d9fe1d308baa65c2e5
Virtus Brædder Opstrup <virtusbradder@gmail.com>
2018-01-15 12:55:11 +0100

    Add project files.

---
+++ H2-OOP-TEC-banking-system/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2_OOP_TEC_banking_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
